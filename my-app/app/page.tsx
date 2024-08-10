0f702ca4672aeb2231df4a45995df4d672f694d3
Ahmad Wafi Idzharulhaqq <167051609+Awfidz@users.noreply.github.com>
2024-08-10 15:50:58 +0700

    feat: add more description in minat and deskripsi singkat section

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -11,17 +11,18 @@ const Home = () => {
       <section className="hero bg-green-300 text-center p-10">
           <Image src="/profilephoto.jpg" alt="Profile Picture" width={300} height={150} className="rounded-full mx-auto" />
           <h1 className="text-4xl font-bold mt-4">Ahmad Wafi Idzharulhaqq</h1>
-          <p className="mt-2 text-lg">Ahmad Wafi lahr di Bandung, 07 Februari 2005. Ia merupakan anak pertama dari tiga bersaudara.
+          <p className="mt-2 text-lg">Ahmad Wafi Idzharulhaqq lahir di Bandung, 07 Februari 2005. Ia merupakan anak pertama dari tiga bersaudara.
             Abi dan Umminya merupakan seorang dosen di universitas swasta. Saat ini, Ia sedang mendalami studi di Institut Teknologi Bandung
-            sebagai mahasiswa jurusan Informatika.
+            sebagai mahasiswa jurusan Informatika. Dalam hidup, Wafi sangat menyukai tantangan tetapi masih sangat kurang dalam mengelola waktu dan mengontrol diri.
           </p>
         </section>
 
         <section className="minatbakat p-10">
           <h2 className="text-3xl font-bold mb-4">Minat</h2>
           <p className="text-lg">Sejak kecil Ia memiliki ketertarikan yang besar dengan pelajaran biologi dan
-            berbagai perangkat elektronik di sekitarnya. Seringkali Ia menghabiskan waktu untuk mengotak atik hal-hal
-            tersebut </p>
+            berbagai perangkat elektronik di sekitarnya. Seringkali Ia menghabiskan waktu untuk mengotak atik perangkat yang ada di hadapannya.
+            Sayangnya, hal ini tidak bertahan cukup lama. Ketika memasuki dunia SMP, Wafi hanya difokuskan pada pelajaran umum dan biologi saja.
+            Saat ini, Ia sedang berusaha menumbuhkan minatnya kembali pada dunia teknologi khususnya di bidang informatika.</p>
         </section>
         
         <section className="Projek p-10">
